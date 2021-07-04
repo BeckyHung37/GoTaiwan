eb4616c9de847cedfa050e8b12655d340ffd440a
BeckyHung37 <beckyhung37@gmail.com>
2021-07-04 22:43:27 +0800

    add firebase to my project

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Link } from "react-router-dom";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCerOcfOq-W_5aSGsZyDeh111uvz0kamc0",
+    authDomain: "gotaiwan-travelmap.firebaseapp.com",
+    projectId: "gotaiwan-travelmap",
+    storageBucket: "gotaiwan-travelmap.appspot.com",
+    messagingSenderId: "609280565263",
+    appId: "1:609280565263:web:522dad66ede45dd1a3037a",
+    measurementId: "G-BQ08VEL7Q1"
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+db.collection("todo").add({
+    text: "Ada",
+    time: Date.now()
+})
+.then((ref) => {
+    console.log("Document ID", ref.id);
+})
+.catch((error) => {
+    console.error("Error adding document: ", error);
+});
+
+
+
+// let app=React.createElement("div",{},"Hello React");
+let app=<div>Hello React Yooo</div>;
+ReactDOM.render(app, document.getElementById("root"));
